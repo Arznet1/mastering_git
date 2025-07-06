60f167ac4c46aadb6193cfd8c1785d58efde7998
arznet <abdulrasheedzakariyau@gail.com>
2025-07-06 10:35:20 +0100

    resolve conflict in stash

--- mastering-git/hello.js
+++ mastering-git/hello.js
@@ -1,5 +1,7 @@
 console.log("hello git");
 console.log("hello git");
-console.log("hello git from feature");
-console.log("this is code is break");
+//my important code to be save
+//another issue come up
+//to be fix urgent 
+//then i unstage my code
 //fix the urgent code that need at once
